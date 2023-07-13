5dafd7e7c45af4215473229ca4fadd32937487f1
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2023-07-13 17:41:24 -0600

    e

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -2,7 +2,7 @@
 
 module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 10_000_000 ) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output wire [6:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
